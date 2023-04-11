bd3a8cdbabe3f5d809a2221ad51e516d4dbddff0
Kennedy <71536021+CodingKen02@users.noreply.github.com>
2023-04-11 18:20:36 -0500

    Update test_userdb.py
    To fix error in CI

--- testing/test_userdb.py
+++ testing/test_userdb.py
@@ -1,7 +1,7 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from path.to.models import Base, User
+#from path.to.models import Base, User
 
 @pytest.fixture
 def session():
