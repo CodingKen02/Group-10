d27638f9c68dcb5dbdeaf7863956cd313781d48c
eta55 <55257097+eta55@users.noreply.github.com>
2023-04-13 14:33:56 -0500

    Update models.py

--- testing/models.py
+++ testing/models.py
@@ -1,6 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-from sqlalchemy import sessionmaker, create_engine
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager
  
