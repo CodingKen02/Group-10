7e1b3265a8b482245e3bcb5d646ebc7692e0da98
MaverickDSmith <andertalley@gmail.com>
2023-04-16 21:10:37 -0500

    lets test this

--- testing/test_database.py
+++ testing/test_database.py
@@ -11,21 +11,9 @@ from source.app import app
 from source.models import *
 from source.instance import *
 
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
 
-@pytest.fixture
-def db_handle():
-    db_fd, db_fname = tempfile.mkstemp()
-    app.app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_fname
-    app.app.config["TESTING"] = True
-    
-    with app.app.app_context():
-        app.db.create_all()
-        
-    yield app.db
-    
-    app.db.session.remove()
-    os.close(db_fd)
-    os.unlink(db_fname)
 
 
 ## Database testing will test whether or not we can ##
@@ -61,20 +49,27 @@ def test_go_to_user_account_page():
 ## The accounts.db should have data already.
 ## Lets test with one of our existing users (myself, Ander)
 
+
+## Since user is already existing
+def test_user_registration():
+    client = app.test_client()
+    response = client.post('register.html', data={'email': 'andertalley@gmail.com', 'password': '1234', 'username': 'andertalley'})
+    assert response.status_code == 200
+
+## If User logins correctly, then the user should be redirected to the home page.
 def test_user_login():
-    response = app.post('/login', data={'email': 'andertalley@gmail.com', 'password': '1234'})
+    client = app.test_client()
+    client.post('register.html', data={'email': 'andertalley@gmail.com', 'password': '1234', 'username': 'andertalley'})
+    response = client.post('/login', data={'email': 'andertalley@gmail.com', 'password': '1234'})
     assert response.status_code == 302
 
-## Testing account page accessS
+## Testing account page access
 def test_user_account_after_login():
     client = app.test_client()
+    client.post('register.html', data={'email': 'andertalley@gmail.com', 'password': '1234', 'username': 'andertalley'})
     response = client.post('/login', data={'email': 'andertalley@gmail.com', 'password': '1234'})
     assert response.status_code == 302
 
     with client:
-        # Log in user before making GET request to /account
-        user = User.query.filter_by(email='andertalley@gmail.com').first()
-        login_user(user)
-
         response = client.get('/account')
         assert response.status_code == 200
