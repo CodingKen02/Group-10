bff692762278cd99f5c24531c4db4afb0243c3de
Kennedy <71536021+CodingKen02@users.noreply.github.com>
2023-05-03 17:50:36 -0500

    Update test_search.py

--- testing/test_search.py
+++ testing/test_search.py
@@ -1,37 +1,44 @@
 ##this will be done in Sprint 4
 
-import re
 import pytest
-from flask import session, request, render_template
-from flask_login import login_user, logout_user
-import sys
-import os
-sys.path.append("source")
-sys.path.append("source/instance")
-from source.models import *
-from source.instance import *
 from app import app, db, Shoe
 
-from sqlalchemy.engine import Engine
-from sqlalchemy import event
+@pytest.fixture(scope='module')
+def test_client():
+    # Set up test client and application context
+    with app.test_client() as client:
+        with app.app_context():
+            # Create test database
+            db.create_all()
 
-# Create a Nike shoe
-nike = Shoe(
-    brand='Nike',
-    name='Air Force 1',
-    price=90,
-    description='The Nike Air Force 1 is a classic shoe.',
-    image_file='air_force_1.jpg'
-)
+            # Create test shoes
+            nike = Shoe(
+                brand='Nike',
+                name='Air Force 1',
+                price=90,
+                description='The Nike Air Force 1 is a classic shoe.',
+                image_file='air_force_1.jpg'
+            )
+            adidas = Shoe(
+                brand='Adidas',
+                name='Superstar',
+                price=80,
+                description='The Adidas Superstar is a classic shoe.',
+                image_file='superstar.jpg'
+            )
 
-# Create an Adidas shoe
-adidas = Shoe(
-    brand='Adidas',
-    name='Superstar',
-    price=80,
-    description='The Adidas Superstar is a classic shoe.',
-    image_file='superstar.jpg'
-)
+            # Add shoes to database
+            db.session.add(nike)
+            db.session.add(adidas)
+            db.session.commit()
+
+            yield client
+
+            # Remove test data
+            db.session.delete(nike)
+            db.session.delete(adidas)
+            db.session.commit()
+            db.drop_all()
 
 @pytest.mark.parametrize("brand, expected_result", [
     ("Nike", [b'Nike']),
@@ -40,11 +47,10 @@ adidas = Shoe(
     ("Jordans", [b'No matching items found.']),
     ("", [b'Nike', b'Adidas'])
 ])
-def test_search(brand, expected_result):
-    client = app.test_client()
-    with client:  
-        response = client.get(f'/search?brand={brand}')
-        assert response.status_code == 200
-        for result in expected_result:
-            assert result in response.data
+def test_search(test_client, brand, expected_result):
+    response = test_client.get(f'/search?brand={brand}')
+    assert response.status_code == 200
+    for result in expected_result:
+        assert result in response.data
+
 
