4dbe620403246032b0abf0ca88f33be0d6661ff1
Kennedy <71536021+CodingKen02@users.noreply.github.com>
2023-05-03 17:13:05 -0500

    Trying again

--- testing/test_search.py
+++ testing/test_search.py
@@ -1,25 +1,20 @@
 ##this will be done in Sprint 4
 
 import re
-from flask import Flask, session, request, render_template, flash, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, current_user, LoginManager, UserMixin, login_required, logout_user
-from models import db, login_manager, User, Shoe, Payment
+import pytest
+from flask import Flask, session, request, render_template
+from flask_login import login_user, logout_user
+import sys
 import os
+import tempfile
+sys.path.append("source")
+sys.path.append("source/instance")
+from source.app import app
+from source.models import *
+from source.instance import *
 
-app = Flask(__name__, static_folder='static')
-
-app.secret_key = 'your-secret-key'
-
-UPLOAD_FOLDER = 'static/uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shoe.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db.init_app(app)
-login_manager.init_app(app)
-login_manager.login_view = 'login'
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
 
 def test_search(client):
     # Test case 1: Search by keyword
