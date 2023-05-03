a083a0bb61f5c2e5eaea6cee7dd43708886de0df
Kennedy <71536021+CodingKen02@users.noreply.github.com>
2023-05-03 17:48:30 -0500

    Update test_search.py

--- testing/test_search.py
+++ testing/test_search.py
@@ -15,11 +15,36 @@ from app import app, db, Shoe
 from sqlalchemy.engine import Engine
 from sqlalchemy import event
 
-def test_search():
+# Create a Nike shoe
+nike = Shoe(
+    brand='Nike',
+    name='Air Force 1',
+    price=90,
+    description='The Nike Air Force 1 is a classic shoe.',
+    image_file='air_force_1.jpg'
+)
+
+# Create an Adidas shoe
+adidas = Shoe(
+    brand='Adidas',
+    name='Superstar',
+    price=80,
+    description='The Adidas Superstar is a classic shoe.',
+    image_file='superstar.jpg'
+)
+
+@pytest.mark.parametrize("brand, expected_result", [
+    ("Nike", [b'Nike']),
+    ("ad", [b'Adidas']),
+    ("Gigabyte", [b'No matching items found.']),
+    ("Jordans", [b'No matching items found.']),
+    ("", [b'Nike', b'Adidas'])
+])
+def test_search(brand, expected_result):
     client = app.test_client()
     with client:  
-        response = client.get('/search?brand=Nike')
+        response = client.get(f'/search?brand={brand}')
         assert response.status_code == 200
-        assert b'Nike' in response.data
-        assert b'Adidas' not in response.data
+        for result in expected_result:
+            assert result in response.data
 
